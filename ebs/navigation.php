0d022fb90b1998eb86799aa680a2b6771482eac9
Bram Vanroy <bramvanroy@hotmail.com>
2016-03-23 13:43:25 +0100

    reinstated the "already done" state on progressbar

--- ebs/navigation.php
+++ ebs/navigation.php
@@ -4,13 +4,14 @@ function goBack() {
 }
 
 $(document).ready(function() {
+    <?php if($step > 1): ?>
+    $(".progressbar li:nth-child(<?php echo $step-1;?>)").addClass("done");
+    <?php endif ?>
     $(".progressbar li:nth-child(<?php echo $step;?>)").addClass("active");
 });
 </script>
-<?php
-echo'
 <nav>
-<ul class="breadcrumb progressbar">
+<ul class="progressbar">
   <li><p>1<span> - Example</span></p></li>
   <li><p>2<span> - Parse</span></p></li>
   <li><p>3<span> - Matrix</span></p></li>
@@ -18,18 +19,12 @@ echo'
   <li><p>5<span> - Query</span></p></li>
   <li><p>6<span> - Results</span></p></li>
 </ul>
-<div id="ccl-logo">';
-
-if ($step==1) {
-  echo '<p><b>GrETEL 2.0</b></p>';
-}
-else {
-  echo '<p><b>GrETEL 2.0</b> - '.$sm.' search mode</p>';
-}
-echo'
-<input type="button" value="Home" onclick="location.href = \'../index.php\'"/>
+<div id="ccl-logo">
+<p>
+    <strong>GrETEL 2.0</strong>
+    <?php if ($step > 1) echo $sm . 'search mode'; ?>
+</p>
+<input type="button" value="Home" onclick="location.href = '../index.php'">
 <a href="http://ccl.kuleuven.be" target="_blank"><img src="../img/ccl-logo-square.png" height="50"></img></a>
 </div>
 </nav>
-';
-?>
