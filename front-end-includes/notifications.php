698bfcbdb0485728505718583ec5ad5e7e8bcf2b
Bram Vanroy <bramvanroy@hotmail.com>
2016-10-03 11:00:57 +0200

    Popup text

--- front-end-includes/notifications.php
+++ front-end-includes/notifications.php
@@ -1,5 +1,5 @@
 <div class="notification-wrapper">
-  <a href="<?php echo $currentPage;?>/results.php#results" title="Go to results">Results found!</a>
+  <a href="<?php echo $currentPage;?>/results.php#results" title="Go to results">Search completed!</a>
   <button title="Close this message">
     <i class="fa fa-times fa-fw" aria-hidden="true"></i>
     <span class="sr-only">Close this notification</span>
