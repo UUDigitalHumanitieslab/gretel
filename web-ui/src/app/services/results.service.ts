22b6ddb6a3678e91fb04d1f7ba3d8c211f38b565
Sheean Spoel <s.j.j.spoel@uu.nl>
2018-04-23 16:04:15 +0200

    Fix for when there is no metadata

--- web-ui/src/app/services/results.service.ts
+++ web-ui/src/app/services/results.service.ts
@@ -1,10 +1,10 @@
-import {Injectable} from '@angular/core';
-import {HttpClient, HttpHeaders, HttpParams} from "@angular/common/http";
-import {DomSanitizer, SafeHtml} from '@angular/platform-browser';
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
-import {Observable} from "rxjs/Observable";
+import { Observable } from "rxjs/Observable";
 
-import {XmlParseService} from './xml-parse.service';
+import { XmlParseService } from './xml-parse.service';
 import 'rxjs/add/operator/mergeMap'
 const url = '/gretel/api/src/router.php/results';
 
@@ -12,22 +12,22 @@ const url = '/gretel/api/src/router.php/results';
 export class ResultsService {
     defaultIsAnalysis = false;
     defaultMetadataFilters: { [key: string]: FilterValue } = {};
-    defaultVariables:{ name: string, path: string }[] = null
+    defaultVariables: { name: string, path: string }[] = null
 
 
     constructor(private http: HttpClient, private sanitizer: DomSanitizer, private xmlParseService: XmlParseService) {
     }
 
     getAllResults(xpath: string,
-                  corpus: string,
-                  components: string[],
-                  retrieveContext: boolean,
-                  isAnalysis = this.defaultIsAnalysis,
-                  metadataFilters = this.defaultMetadataFilters,
-                  variables = this.defaultVariables): Observable<any> {
+        corpus: string,
+        components: string[],
+        retrieveContext: boolean,
+        isAnalysis = this.defaultIsAnalysis,
+        metadataFilters = this.defaultMetadataFilters,
+        variables = this.defaultVariables): Observable<any> {
         return Observable.create(async observer => {
             let offset = 0;
-            while(!observer.closed){
+            while (!observer.closed) {
                 await this.results(xpath, corpus, components, offset, retrieveContext, isAnalysis, metadataFilters, variables)
                     .then((res) => {
                         if (res) {
@@ -55,13 +55,13 @@ export class ResultsService {
      * @param variables Named variables to query on the matched hit (can be determined using the Extractinator)
      */
     async results(xpath: string,
-                  corpus: string,
-                  components: string[],
-                  offset: number = 0,
-                  retrieveContext: boolean,
-                  isAnalysis = this.defaultIsAnalysis,
-                  metadataFilters = this.defaultMetadataFilters,
-                  variables = this.defaultVariables) {
+        corpus: string,
+        components: string[],
+        offset: number = 0,
+        retrieveContext: boolean,
+        isAnalysis = this.defaultIsAnalysis,
+        metadataFilters = this.defaultMetadataFilters,
+        variables = this.defaultVariables) {
         const httpOptions = {
             headers: new HttpHeaders({
                 'Content-Type': 'application/json',
@@ -140,7 +140,7 @@ export class ResultsService {
 
     private mapMeta(data: {
         metadata: {
-            meta: {
+            meta?: {
                 $: {
                     type: string,
                     name: string,
@@ -149,7 +149,7 @@ export class ResultsService {
             }[]
         }
     }): Hit['metaValues'] {
-        return data.metadata.meta.reduce((values, meta) => {
+        return !data.metadata.meta ? {} : data.metadata.meta.reduce((values, meta) => {
             values[meta.$.name] = meta.$.value;
             return values;
         }, {});
@@ -235,7 +235,7 @@ type ApiSearchResult = [
     { [id: string]: string },
     // 7 end pos iteration
     number
-    ];
+];
 
 export interface SearchResults {
     hits: Hit[],
