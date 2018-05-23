7cb02758908f0aa3942e29977fbedf8caea66e49
Sheean Spoel <s.j.j.spoel@uu.nl>
2018-05-23 14:57:54 +0200

    Show all matrix options

--- web-ui/src/app/components/step/matrix/matrix.component.ts
+++ web-ui/src/app/components/step/matrix/matrix.component.ts
@@ -11,14 +11,51 @@ export class MatrixComponent implements OnInit {
     public xml: string;
     public xpath: string;
     public tokens: string[];
+    public showAdvanced: boolean;
 
     public parts = [
-        { label: "Word" },
-        { label: "Lemma" },
-        { label: "Word class" },
-        { label: "Detailed word class" },
-        { label: "Optional" },
-        { label: "Exclude" }]
+        {
+            label: "Word",
+            description: "The exact word form (also known as token).",
+            value: "token",
+            advanced: false
+        },
+        {
+            label: "Word (case-sensitive)",
+            description: "The word form must match exactly, including the casing.",
+            value: "cs",
+            advanced: true
+        },
+        {
+            label: "Lemma",
+            description: "Word form that generalizes over inflected forms. For example: gaan is the lemma of ga, gaat, gaan, ging, gingen, and gegaan.",
+            value: "lemma",
+            advanced: false
+        },
+        {
+            label: "Word class",
+            description: "Short Dutch part-of-speech tag. The different tags are: n (noun), ww (verb), adj (adjective), lid (article), vnw (pronoun), vg (conjunction), bw (adverb), tw (numeral), vz (preposition), tsw (interjection), spec (special token), and let (punctuation).",
+            value: "",
+            advanced: false
+        },
+        {
+            label: "Detailed word class",
+            description: "Long part-of-speech tag. For example: N(soort,mv,basis), WW(pv,tgw,ev), VNW(pers,pron,nomin,vol,2v,ev).",
+            value: "postag",
+            advanced: true
+        },
+        {
+            label: "Optional",
+            description: "The word will be ignored in the search instruction. It may be included in the results, but it is not required that it is present.",
+            value: "na",
+            advanced: false
+        },
+        {
+            label: "Exclude",
+            description: "The word class and the dependency relation will be explicitly excluded from the results.",
+            value: "not",
+            advanced: true
+        }];
 
     // TODO: don't reparse
     @Input('sentence')
