bbc15ba7f52eab38d6974e4f4a9bfb9f0edfd125
Sheean Spoel <s.j.j.spoel@uu.nl>
2018-06-18 17:22:53 +0200

    Removed unneeded files. Added favicon #46

--- config-example.php
+++ config-example.php
@@ -2,7 +2,8 @@
 
 function buildHomeURL()
 {
-    $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
+    $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? 'https://' : 'http://';
+
     return $protocol.$_SERVER['HTTP_HOST'].'/'.basename(__DIR__).'/';
 }
 
@@ -10,24 +11,24 @@ defined('ROOT_PATH') or define('ROOT_PATH', __DIR__);
 defined('HOME_PATH') or define('HOME_PATH', buildHomeURL());
 
 // Whether or not to use an API to retrieve uploaded corpora. If empty, the corpora will need to be defined in the code.
-defined('API_URL')   or define('API_URL',   '');
+defined('API_URL') or define('API_URL', '');
 
 // === CHANGE PATH TO ALPINO DIRECTORY === //
-$alpinoDirectory = ROOT_PATH."/parsers/Alpino";
+$alpinoDirectory = ROOT_PATH.'/parsers/Alpino';
 
 // In the recursive Javascript call, how many sentences to flush each time
-$flushLimit = 1;
+$flushLimit = 50;
 // Global limit on the max amount of sentences returned
 $resultsLimit = 500;
 // Global limit on the max amount of sentences returned for analysis
 $analysisLimit = 50000;
 
-$cats = array("advp", "ahi", "ap", "conj", "cp", "detp", "du", "inf", "list", "mwu",
-    "np", "oti", "pp", "ppart", "ppres", "rel", "smain", "ssub", "sv1", "svan", "ti", "top", "whq", "whrel", "whsub");
+$cats = array('advp', 'ahi', 'ap', 'conj', 'cp', 'detp', 'du', 'inf', 'list', 'mwu',
+    'np', 'oti', 'pp', 'ppart', 'ppres', 'rel', 'smain', 'ssub', 'sv1', 'svan', 'ti', 'top', 'whq', 'whrel', 'whsub', );
 
 $ebsPages = array(
     'input.php' => 'Example',
-    'parse.php'=> 'Parse',
+    'parse.php' => 'Parse',
     'matrix.php' => 'Matrix',
     'tb-sel.php' => 'Treebanks',
     'query.php' => 'Query',
@@ -53,8 +54,8 @@ $matrixOptions = array(
     ),
     array(
       'optional in search' => 'na',
-      'not in search' => 'not'
-    )
+      'not in search' => 'not',
+    ),
 );
 
 // BaseX database variables
@@ -67,36 +68,36 @@ $databaseGroups = array(
         'WRPEC', 'WRPEE', 'WRPEF', 'WRPEG', 'WRPEH', 'WRPEI',
         'WRPEK', 'WRPEL', 'WRPPC', 'WRPPD', 'WRPPE', 'WRPPF',
         'WRPPI', 'WRPPJ', 'WRPPK', 'WRUEA', 'WRUED', 'WRUEE',
-        'WSUEA', 'WSUTB'
+        'WSUEA', 'WSUTB',
       ),
       'machine' => 'machinename',
-      'port' => 0000
+      'port' => 0000,
     ),
     'WRPEA' => array(
       'machine' => 'machinename',
       'port' => 0000,
-      'sentences' => 4395094
+      'sentences' => 4395094,
     ),
     'WRPEJ' => array(
       'machine' => 'machinename',
       'port' => 0000,
-      'sentences' => 1354245
+      'sentences' => 1354245,
     ),
-    'WRPPB' =>  array(
+    'WRPPB' => array(
       'machine' => 'machinename',
       'port' => 0000,
-      'sentences' => 1709808
+      'sentences' => 1709808,
     ),
     'WRPPG' => array(
       'machine' => 'machinename',
       'port' => 0000,
-      'sentences' => 14973209
+      'sentences' => 14973209,
     ),
     'WRPPH' => array(
       'machine' => 'machinename',
       'port' => 0000,
-      'sentences' => 5475556
-    )
+      'sentences' => 5475556,
+    ),
   ),
   'cgn' => array(
     'components' => array(
@@ -104,21 +105,21 @@ $databaseGroups = array(
       'NE', 'VE', 'NF', 'VF', 'NG', 'VG',
       'NH', 'VH', 'VI', 'NI', 'VJ', 'NJ', 'NK', 'VK',
       'NL', 'VL', 'NM', 'VM', 'NN', 'VN',
-      'NO', 'VO'
+      'NO', 'VO',
     ),
     'machine' => 'machinename',
-    'port' => 0000
+    'port' => 0000,
   ),
   'lassy' => array(
     'components' => array(
-      'DPC', 'WIKI', 'WRPE', 'WRPP', 'WSU'
+      'DPC', 'WIKI', 'WRPE', 'WRPP', 'WSU',
     ),
     'machine' => 'machinename',
-    'port' => 0000
+    'port' => 0000,
   ),
   'api' => array(
     'machine' => 'machinename',
-    'port' => 0000
+    'port' => 0000,
   ),
 );
 
