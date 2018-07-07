d1c22fdfe1e36991b79e6f27e5c97d56a1e2bcb3
Bram.Vanroy@UGent.be <Bram.Vanroy@UGent.be>
2018-07-07 16:42:26 +0200

    Merged updated corpora config from Leuven

--- front-end-includes/tb-sel-shared-content.php
+++ front-end-includes/tb-sel-shared-content.php
@@ -1,5 +1,5 @@
 <?php 
-require "xpath-variables-hidden.php";
+require 'xpath-variables-hidden.php';
 ?>
 <p>Which treebank do you want to query? Click on the treebank name to see its different components. If you would like to get more information
   on these treebanks, you can find the project websites in <a href="documentation.php#faq-3" target="_blank" title="More information on corpora">our FAQ</a>.</p>
@@ -12,38 +12,24 @@ require "xpath-variables-hidden.php";
 ?>
 
 <form action="<?php echo $nextPage; ?>" method="post">
-  <?php render_xpath_variables_hidden("xpath-variables"); ?>
+  <?php render_xpath_variables_hidden('xpath-variables'); ?>
   <div class="flex-content">
     <div class="labels-wrapper">
-      <?php if (!API_URL) { ?>
-      <div class="label-wrapper">
-        <label>
-          <input type="radio" name="treebank" value="cgn" required> <strong>CGN treebank</strong>: spoken Dutch - version 2.0.1
-        </label>
-      </div>
-      <div class="label-wrapper">
-        <label>
-          <input type="radio" name="treebank" value="lassy" required> <strong>LASSY Small</strong>: written Dutch - version 1.1
-        </label>
-      </div>
-      <div class="label-wrapper">
-        <label>
-          <input type="radio" name="treebank" value="sonar" required> <strong>SoNaR treebank</strong>: written Dutch - version 1.0
-        </label>
-      </div>
-      <?php } ?>
+      <?php
+      foreach ($databaseGroups as $corpus => $arr) {
+          echo "<div class=\"label-wrapper\">
+          <label>
+            <input type=\"radio\" name=\"treebank\" value=\"{$corpus}\" data-multioption=\"{$arr['multioption']}\">
+            <strong>{$arr['fullName']}</strong>: {$arr['production']} {$arr['language']} - version {$arr['version']}
+          </label>
+        </div>";
+      } ?>
     </div>
   </div>
   <div class="corpora-wrapper">
-      <?php if (!API_URL) { ?>
-      <div class="cgn" style="display:none">
-        <?php require ROOT_PATH."/front-end-includes/tb-cgn.php"; ?>
-      </div>
-      <div class="lassy" style="display:none">
-        <?php require ROOT_PATH."/front-end-includes/tb-lassy.php"; ?>
-      </div>
-      <div class="sonar" style="display:none">
-        <?php require ROOT_PATH."/front-end-includes/tb-sonar.php"; ?>
-      </div>
-      <?php } ?>
+    <?php foreach ($databaseGroups as $corpus => $arr) {
+          echo "<div class=\"{$corpus}\" style=\"display:none\">";
+          require ROOT_PATH."/front-end-includes/tb-{$corpus}.php";
+          echo '</div>';
+      } ?>
   </div>
