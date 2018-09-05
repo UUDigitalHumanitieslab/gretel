9fe8e2ccdeda130a908d783b00ff31ab40af0c5f
Sheean Spoel <s.j.j.spoel@uu.nl>
2018-09-05 12:27:13 +0200

    Retain tree visualization when tree didn't change. Resolves #141

--- web-ui/src/app/components/tree-visualizer/tree-visualizer.component.ts
+++ web-ui/src/app/components/tree-visualizer/tree-visualizer.component.ts
@@ -1,53 +1,74 @@
-import { Component, ElementRef, EventEmitter, Input, OnInit, OnChanges, Output, ViewChild } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, OnInit, OnChanges, Output, ViewChild, SimpleChange } from '@angular/core';
 import * as $ from 'jquery';
 import './tree-visualizer';
+
+type TypedChanges = { [name in keyof TreeVisualizerComponent]: SimpleChange };
+type TreeVisualizerDisplay = 'fullscreen' | 'inline' | 'both';
 
 @Component({
     selector: 'grt-tree-visualizer',
     templateUrl: './tree-visualizer.component.html',
     styleUrls: ['./tree-visualizer.component.scss']
 })
-export class TreeVisualizerComponent implements OnChanges {
+export class TreeVisualizerComponent implements OnChanges, OnInit {
+    @ViewChild('output', { read: ElementRef })
+    public output: ElementRef;
+
     @Input()
     public xml: string;
 
     @Input()
-    public display: 'fullscreen' | 'inline' | 'both' = 'inline';
+    public display: TreeVisualizerDisplay = 'inline';
 
     @Input()
     public fullScreenButton = true;
 
     @Output()
-    public onDisplayChange = new EventEmitter<TreeVisualizerComponent["display"]>();
+    public onDisplayChange = new EventEmitter<TreeVisualizerDisplay>();
 
     constructor() {
     }
 
-    ngOnChanges() {
-        let element: any = $(`.output`);
-        element.treeVisualizer(this.xml, {
-            nvFontSize: 14,
-            noFsButton: !this.fullScreenButton
-        });
+    ngOnInit() {
+        let element = $(this.output.nativeElement);
+        this.visualize(element);
         element.on('close', () => {
             if (this.display == 'both') {
                 this.onDisplayChange.next('inline')
             }
         });
-        this.updateVisibility();
+        this.updateVisibility(element);
     }
 
-    private updateVisibility() {
+    ngOnChanges(changes: TypedChanges) {
+        let element = $(this.output.nativeElement);
+        if (changes.xml && changes.xml.currentValue != changes.xml.previousValue) {
+            this.visualize(element);
+        }
+        this.updateVisibility(element);
+    }
+
+    private visualize(element: any) {
+        element.treeVisualizer(this.xml, {
+            nvFontSize: 14,
+            noFsButton: !this.fullScreenButton
+        });
+    }
+
+    private updateVisibility(element: JQuery<Element>) {
+        let inline = element.children('.tree-visualizer');
+        let fullscreen = element.children('.tree-visualizer-fs');
+
         if (this.display != 'fullscreen') {
-            $('.tree-visualizer').show();
+            inline.show();
         } else {
-            $('.tree-visualizer').hide();
+            inline.hide();
         }
 
         if (this.display != 'inline') {
-            $('.tree-visualizer-fs').show();
+            fullscreen.show();
         } else {
-            $('.tree-visualizer-fs').hide();
+            fullscreen.hide();
         }
     }
 }
