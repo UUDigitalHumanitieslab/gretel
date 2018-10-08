c891ad363a8715a5a10e916a202037d3de46be22
Sheean Spoel <sheean@sheean.nl>
2018-10-08 15:56:37 +0200

    Shows the metadata of a document (if there is any). Resolves #161

--- web-ui/src/app/components/tree-visualizer/tree-visualizer.component.ts
+++ web-ui/src/app/components/tree-visualizer/tree-visualizer.component.ts
@@ -1,13 +1,25 @@
-import { Component, ElementRef, EventEmitter, Input, OnInit, OnChanges, Output, ViewChild, SimpleChange, SecurityContext } from '@angular/core';
+import {
+    Component,
+    ElementRef,
+    EventEmitter,
+    Input,
+    OnInit,
+    OnChanges,
+    Output,
+    SecurityContext,
+    SimpleChange,
+    ViewChild,
+} from '@angular/core';
 import { SafeHtml, DomSanitizer } from '@angular/platform-browser';
 
 import * as $ from 'jquery';
 
-import { DownloadService } from '../../services/_index';
+import { DownloadService, XmlParseService } from '../../services/_index';
 import './tree-visualizer';
 
 type TypedChanges = { [name in keyof TreeVisualizerComponent]: SimpleChange };
 type TreeVisualizerDisplay = 'fullscreen' | 'inline' | 'both';
+interface Metadata { name: string; value: string; }
 
 @Component({
     selector: 'grt-tree-visualizer',
@@ -42,26 +54,28 @@ export class TreeVisualizerComponent implements OnChanges, OnInit {
     public url: string;
 
     @Output()
-    public onDisplayChange = new EventEmitter<TreeVisualizerDisplay>();
+    public displayChange = new EventEmitter<TreeVisualizerDisplay>();
+
+    public metadata: Metadata[] | undefined;
 
     // jquery tree visualizer
     private instance: any;
 
-    constructor(private sanitizer: DomSanitizer, private downloadService: DownloadService) {
+    constructor(private sanitizer: DomSanitizer, private downloadService: DownloadService, private xmlParseService: XmlParseService) {
     }
 
     ngOnInit() {
-        let element = $(this.output.nativeElement);
+        const element = $(this.output.nativeElement);
         element.on('close', () => {
-            if (this.display == 'both') {
-                this.onDisplayChange.next('inline')
+            if (this.display === 'both') {
+                this.displayChange.next('inline');
             }
         });
     }
 
     ngOnChanges(changes: TypedChanges) {
-        let element = $(this.output.nativeElement);
-        if (changes.xml && changes.xml.currentValue != changes.xml.previousValue) {
+        const element = $(this.output.nativeElement);
+        if (changes.xml && changes.xml.currentValue !== changes.xml.previousValue) {
             this.visualize(element);
         }
 
@@ -86,23 +100,42 @@ export class TreeVisualizerComponent implements OnChanges, OnInit {
                 showMatrixDetails: this.showMatrixDetails
             });
 
+            this.xmlParseService.parse(this.xml).then((data) => {
+                this.showMetadata(data);
+            });
             this.updateVisibility();
         });
     }
 
     private updateVisibility() {
-        let inline = $(this.inlineRef && this.inlineRef.nativeElement);
+        const inline = $(this.inlineRef && this.inlineRef.nativeElement);
 
-        if (this.display != 'fullscreen') {
+        if (this.display !== 'fullscreen') {
             inline.show();
         } else {
             inline.hide();
         }
 
-        if (this.display != 'inline') {
+        if (this.display !== 'inline') {
             this.instance.trigger('open-fullscreen');
         } else {
             this.instance.trigger('close-fullscreen');
         }
+    }
+
+    /**
+     * Shows the metadata of a tree.
+     * @param data The parsed XML data
+     */
+    private showMetadata(data: any) {
+        const result: Metadata[] = [];
+        if (data && data.alpino_ds && data.alpino_ds.metadata) {
+            for (const item of data.alpino_ds.metadata[0].meta.sort(function (a, b) {
+                return a.$.name.localeCompare(b.$.name);
+            })) {
+                result.push({ name: item.$.name, value: item.$.value });
+            }
+        }
+        this.metadata = result.length ? result : undefined;
     }
 }
