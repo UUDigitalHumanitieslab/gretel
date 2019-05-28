33b06af74c634b09f861ad8ccb57201d9468933c
Sheean Spoel <s.j.j.spoel@uu.nl>
2019-05-28 15:00:45 +0200

    Usable result downloads

--- web-ui/src/app/services/download.service.ts
+++ web-ui/src/app/services/download.service.ts
@@ -2,6 +2,7 @@ import { Injectable, SecurityContext } from '@angular/core';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { saveAs } from 'file-saver';
 import { Hit } from './results.service';
+import * as JSZip from 'jszip';
 
 @Injectable()
 export class DownloadService {
@@ -15,31 +16,45 @@ export class DownloadService {
         hits: number,
         sentences: string
     }[]) {
-        let rows = [this.formatCsvRow(['Provider', 'Treebank', 'Component', 'Hits', 'All Sentences'])];
-        for (let count of counts) {
-            rows.push(this.formatCsvRow([count.provider, count.corpus, count.component, count.hits, count.sentences]));
+        const rows = [this.formatCsvRow('Provider', 'Treebank', 'Component', 'Hits', 'All Sentences')];
+        for (const count of counts) {
+            rows.push(this.formatCsvRow(count.provider, count.corpus, count.component, count.hits, count.sentences));
         }
 
         this.downloadRows('gretel-distribution.csv', 'text/csv', rows);
     }
 
-    public downloadResults(results: {corpus: string, components: string[], xpath: string, hits: Hit[]}[]) {
-        let rows: string[] = [];
-        results.forEach(({corpus, components, xpath, hits}) => {
-            rows.push(
-`Corpus: ${corpus}
+    public async downloadResults(results: { corpus: string, components: string[], xpath: string, hits: Hit[] }[]) {
+        const zip = new JSZip();
+        const corporaNames: string[] = [];
+        results.forEach(({ corpus, components, xpath, hits }) => {
+            zip.file(
+                `${corpus}.meta.txt`,
+                this.blob(
+                    'text/plain',
+                    [`Corpus: ${corpus}
 Components: ${components.join('-')}
 XPath: ${xpath.replace(/\n/g, '\t')}
 Date: ${new Date()}
-`);
+`]));
+            corporaNames.push(corpus);
 
-                for (let i = 0; i < hits.length; i++) {
-                    let hit = hits[i];
-                    rows.push(`${i + 1}\t${hit.fileId}\t${corpus}\t${hit.component}\t${this.highlightSentence(hit.highlightedSentence)}`);
-                }
-        })
+            const rows: string[] = [this.formatCsvRow('#', 'ID', 'Corpus', 'Component', 'Sentence')];
 
-        this.downloadRows('gretel-results.txt', 'text/plain', rows);
+            for (let i = 0; i < hits.length; i++) {
+                const hit = hits[i];
+                rows.push(this.formatCsvRow(
+                    i + 1,
+                    hit.fileId,
+                    corpus,
+                    hit.component,
+                    this.highlightSentence(hit.highlightedSentence)));
+            }
+
+            zip.file(`${corpus}.csv`, this.blob('text/csv', rows));
+        });
+        const content = await zip.generateAsync({ type: 'blob' });
+        saveAs(content, `gretel-${corporaNames.join('-')}-${this.timestamp()}.zip`);
     }
 
     public downloadXPath(xpath: string) {
@@ -50,11 +65,22 @@ Date: ${new Date()}
         saveAs(new Blob([xml], { type: `text/xml;charset=utf-8` }), filename);
     }
 
-    public downloadFilelist(filenames: string[], name: string, header: string = undefined, extension: string = '.fl') {
+    public downloadFilelist(filenames: string[], name: string, header?: string, extension: string = '.fl') {
         if (!header) {
-            header = name
+            header = name;
         }
-        saveAs(new Blob([`${header}\n${filenames.join('\n')}`], { type: 'text/txt' }), `${name}${extension}`)
+        saveAs(new Blob([`${header}\n${filenames.join('\n')}`], { type: 'text/txt' }), `${name}${extension}`);
+    }
+
+    private timestamp() {
+        const now = new Date();
+        return [now.getUTCFullYear(),
+        (now.getUTCMonth() + 1).toString().padStart(2, '0'),
+        now.getUTCDate().toString().padStart(2, '0'),
+        now.getUTCHours().toString().padStart(2, '0'),
+        now.getUTCMinutes().toString().padStart(2, '0'),
+        now.getUTCSeconds().toString().padStart(2, '0'),
+        ].join('');
     }
 
     private highlightSentence(highlightedSentence: SafeHtml) {
@@ -63,11 +89,15 @@ Date: ${new Date()}
             .replace(/<\/strong>/g, '</hit>');
     }
 
-    private formatCsvRow(data: any[]) {
+    private formatCsvRow(...data: any[]) {
         return data.join(',') + '\n';
     }
 
-    private downloadRows(filename: string, fileType: "text/csv" | "text/plain", rows: string[]) {
-        saveAs(new Blob(rows, { type: `${fileType};charset=utf-8` }), filename);
+    private blob(fileType: 'text/csv' | 'text/plain', rows: string[]) {
+        return new Blob(rows, { type: `${fileType};charset=utf-8`, endings: 'native' });
+    }
+
+    private downloadRows(filename: string, fileType: 'text/csv' | 'text/plain', rows: string[]) {
+        saveAs(this.blob(fileType, rows), filename);
     }
 }
