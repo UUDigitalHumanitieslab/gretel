c8acfe8ea73fabac5e8225447f5b8d86bfc2e784
Sheean Spoel <sheean@sheean.nl>
2019-06-16 22:55:19 +0200

    Polyfill for flatMap() in Edge

--- web-ui/src/polyfills.ts
+++ web-ui/src/polyfills.ts
@@ -64,3 +64,6 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+
+// flatMap support in Edge
+import 'array-flat-polyfill';
