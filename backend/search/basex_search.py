044c7076b10764f70813ebd47e608a9dc2df5b86
Tijmen Baarda <tijmenbaarda@gmail.com>
2022-07-22 14:48:00 +0200

    Check for validity of function input

--- backend/search/basex_search.py
+++ backend/search/basex_search.py
@@ -1,20 +1,43 @@
 """Auxiliary functions to facilitate searching in BaseX."""
 
+import lxml.etree
+
+ALLOWED_DBNAME_CHARS = '!#$%&\'()+-=@[]^_`{}~ABCDEFGHIJKLMNOPQRSTUVWXYZabcde' \
+                       'fghijklmnopqrstuvwxyz0123456789'
+
+
+def check_xpath(xpath: str) -> bool:
+    """Return True if a string is (only) a valid XPath, otherwise False."""
+    try:
+        lxml.etree.XPath(xpath)
+    except lxml.etree.XPathError:
+        return False
+    else:
+        return True
+
+
+def check_db_name(db_name: str) -> bool:
+    """Return True if a string may be (only) a valid BaseX database name,
+    otherwise False."""
+    return all(x in ALLOWED_DBNAME_CHARS for x in db_name)
+
 
 def generate_xquery_search(basex_db: str, xpath: str) -> str:
     """Return XQuery string for use in BaseX to get all occurances
     of a given XPath in XML format in a given BaseX database."""
+    if not check_db_name(basex_db) or not check_xpath(xpath):
+        raise ValueError
     query = 'for $node in db:open("' + basex_db + '")/treebank' \
             + xpath + \
-            'let $tree := ($node/ancestor::alpino_ds)' \
-            'let $sentid := ($tree/@id)' \
-            'let $sentence := ($tree/sentence)' \
-            'let $ids := ($node//@id)' \
-            'let $indexs := (distinct-values($node//@index))' \
-            'let $indexed := ($tree//node[@index=$indexs])' \
-            'let $begins := (($node | $indexed)//@begin)' \
-            'let $beginlist := (distinct-values($begins))' \
-            'let $meta := ($tree/metadata/meta)' \
+            ' let $tree := ($node/ancestor::alpino_ds)' \
+            ' let $sentid := ($tree/@id)' \
+            ' let $sentence := ($tree/sentence)' \
+            ' let $ids := ($node//@id)' \
+            ' let $indexs := (distinct-values($node//@index))' \
+            ' let $indexed := ($tree//node[@index=$indexs])' \
+            ' let $begins := (($node | $indexed)//@begin)' \
+            ' let $beginlist := (distinct-values($begins))' \
+            ' let $meta := ($tree/metadata/meta)' \
             ' return <match>{data($sentid)}||{data($sentence)}' \
             '||{string-join($ids, \'-\')}||' \
             '{string-join($beginlist, \'-\')}||{$node}||{$meta}' \
@@ -27,5 +50,7 @@ def generate_xquery_search(basex_db: str, xpath: str) -> str:
 def generate_xquery_count(basex_db: str, xpath: str) -> str:
     """Return XQuery string for use in BaseX to get the count of all
     occurances of a given XPath in a given BaseX database."""
+    if not check_db_name(basex_db) or not check_xpath(xpath):
+        raise ValueError
     return 'count(for $node in db:open("{}")/treebank{} return $node)' \
         .format(basex_db, xpath)
--- backend/search/tests.py
+++ backend/search/tests.py
@@ -1,3 +1,39 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .basex_search import (check_db_name, check_xpath, generate_xquery_search,
+                           generate_xquery_count)
+
+
+class BaseXSearchTestCase(TestCase):
+    DB_NAME_CHECK = 'EUROPARL_ID_EP-00_0000'
+    XPATH_CHECK = '//node[@cat="smain" and node[@rel="su" and @pt="vnw"] and' \
+                  ' node[@rel="hd" and @pt="ww"] and node[@rel="predc" and' \
+                  ' @cat="np" and node[@rel="det" and @pt="lid"] and' \
+                  ' node[@rel="hd" and @pt="n"]]]'
+
+    def test_check_db_name(self):
+        self.assertTrue(check_db_name(self.DB_NAME_CHECK))
+        self.assertFalse(check_db_name(self.DB_NAME_CHECK + '") let $a := 0'))
+
+    def test_check_xpath(self):
+        self.assertTrue(check_xpath(self.XPATH_CHECK))
+        self.assertFalse(check_xpath(self.XPATH_CHECK + ' let $a := 0'))
+
+    def test_xquery_search_count(self):
+        # Check if function runs without error
+        generate_xquery_search(self.DB_NAME_CHECK, self.XPATH_CHECK)
+        generate_xquery_count(self.DB_NAME_CHECK, self.XPATH_CHECK)
+        # Illegal arguments should raise error
+        for func in (generate_xquery_search, generate_xquery_count):
+            self.assertRaises(
+                ValueError,
+                func,
+                self.DB_NAME_CHECK + ' ',
+                self.XPATH_CHECK
+            )
+            self.assertRaises(
+                ValueError,
+                func,
+                self.DB_NAME_CHECK,
+                self.XPATH_CHECK + ' let $a := 0'
+            )
