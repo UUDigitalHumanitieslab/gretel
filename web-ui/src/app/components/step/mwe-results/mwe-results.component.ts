bd146483258bd5c72a555b2d6749346da88b8f68
ben <b.bonfil@uu.nl>
2022-07-27 15:48:16 +0200

    allow search for custom expressions (not in list of canonical forms)

--- web-ui/src/app/components/step/mwe-results/mwe-results.component.ts
+++ web-ui/src/app/components/step/mwe-results/mwe-results.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { ResultsComponent } from '../results/results.component';
 
 @Component({
@@ -9,6 +9,9 @@ import { ResultsComponent } from '../results/results.component';
 
 })
 export class MweResultsComponent extends ResultsComponent {
+    @Input()
+    public canSaveQuery: boolean = true;
+
     @Output()
     public saveQuery = new EventEmitter<string>();
 
