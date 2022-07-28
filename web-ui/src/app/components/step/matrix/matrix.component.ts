bd35fc024b23c4cecd56a810e2eacfc2513724fd
Sheean Spoel <s.j.j.spoel@uu.nl>
2022-07-28 16:03:15 +0200

    Refactored matrix logic

--- web-ui/src/app/components/step/matrix/matrix.component.ts
+++ web-ui/src/app/components/step/matrix/matrix.component.ts
@@ -4,28 +4,20 @@ import { ConfirmationService } from 'primeng/api';
 
 import { ValueEvent } from 'lassy-xpath';
 
-import { StateService, TokenAttributes } from '../../../services/_index';
+import { StateService } from '../../../services/_index';
 import { StepDirective } from '../step.directive';
-import { StepType, GlobalStateExampleBased, DefaultTokenAttributes } from '../../../pages/multi-step-page/steps';
+import { StepType, GlobalStateExampleBased } from '../../../pages/multi-step-page/steps';
 import { NotificationService } from '../../../services/notification.service';
 import { animations } from '../../../animations';
-import { Option, options } from './matrix-option.component';
-
-const optionsLookup = Object.assign(
-    {},
-    ...options.map(option => ({ [option.value]: option }))) as { [key: string]: Option };
+import { matrixOptions, Matrix, MatrixOption, TokenAttributes, TokenDependents } from '../../../models/matrix';
 
 interface IndexedToken {
     /**
      * string value of this token
      */
     value: string,
-    index: number,
-    /**
-     * the key of the exclusive option set for this token (or undefined)
-     */
-    exclusive: keyof TokenAttributes
-};
+    index: number
+}
 
 @Component({
     animations,
@@ -36,67 +28,12 @@ interface IndexedToken {
 export class MatrixComponent extends StepDirective<GlobalStateExampleBased> implements OnInit, OnDestroy {
     private warningId: number;
     private subscriptions: Subscription[];
+    private matrix: Matrix;
+
     public stepType = StepType.Matrix;
 
-    private set attributes(tokens: TokenAttributes[]) {
-        let alwaysAdvanced = false;
-        let updatedTokens = false;
-
-        for (let i in tokens) {
-            let token = tokens[i];
-            if (token != null) {
-                for (let [key, value] of Object.entries(token)) {
-                    var option = optionsLookup[key];
-                    if (option.advanced) {
-                        if (value !== DefaultTokenAttributes[key]) {
-                            this.showAdvanced = true;
-                            alwaysAdvanced = true;
-                        }
-                    }
-
-                    if (this.indexedTokens) {
-                        if (option.exclusive) {
-                            if (value === true || value === 'include') {
-                                // checked exclusive option
-                                if (this.indexedTokens[i].exclusive !== option.value) {
-                                    updatedTokens = true;
-                                }
-                                this.indexedTokens[i].exclusive = option.value;
-                            } else {
-                                // unchecked exclusive option
-                                if (this.indexedTokens[i].exclusive == option.value) {
-                                    updatedTokens = true;
-                                }
-                                this.indexedTokens[i].exclusive = undefined;
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-        if (updatedTokens) {
-            // hello change detection
-            this.indexedTokens = [...this.indexedTokens];
-        }
-
-        this.alwaysAdvanced = alwaysAdvanced;
-
-        if (this.tokenValues &&
-            tokens.length === this.tokenValues.length) {
-            // only update the values, but prevent the whole array
-            // from being re-rendered
-            for (let i = 0; i < tokens.length; i++) {
-                Object.assign(this.tokenValues[i], tokens[i]);
-            }
-        } else {
-            this.tokenValues = tokens;
-        }
-    }
-
-    private get attributes() {
-        return this.tokenValues;
-    }
+    public attributes: TokenAttributes[];
+    public tokenDependents: TokenDependents[];
 
     public set tokens(values: string[]) {
         if (this.indexedTokens &&
@@ -107,7 +44,7 @@ export class MatrixComponent extends StepDirective<GlobalStateExampleBased> impl
                 this.indexedTokens[i].value = values[i];
             }
         } else {
-            this.indexedTokens = values.map((value, index) => ({ value, index, exclusive: undefined }));
+            this.indexedTokens = values.map((value, index) => ({ value, index }));
         }
         this.filename = values.filter(t => t.match(/[^'"-:!?,\.]/)).join('-').toLowerCase() + '.xml';
     }
@@ -138,9 +75,7 @@ export class MatrixComponent extends StepDirective<GlobalStateExampleBased> impl
      */
     public alwaysAdvanced: boolean;
 
-    public tokenValues: TokenAttributes[];
-
-    public options = options;
+    public options = matrixOptions;
     public explanation: string = undefined;
 
     private originalXPath: string;
@@ -151,7 +86,6 @@ export class MatrixComponent extends StepDirective<GlobalStateExampleBased> impl
         super(stateService);
         this.subscriptions = [
             this.state$.subscribe(state => {
-                this.attributes = state.attributes;
                 this.ignoreTopNode = state.ignoreTopNode;
                 this.isCustomXPath = state.isCustomXPath;
                 this.respectOrder = state.respectOrder;
@@ -160,40 +94,39 @@ export class MatrixComponent extends StepDirective<GlobalStateExampleBased> impl
                 this.tokens = state.tokens;
                 this.xpath = state.xpath;
                 this.loading = state.loading;
+
+                let first = false;
+                if (this.matrix === undefined) {
+                    this.matrix = Matrix.default(state.tokens.length);
+                    // make sure a new matrix is initialized
+                    first = true;
+                }
+                if (this.matrix.setMultiple(state.attributes) || first) {
+                    this.updateMatrix();
+                }
             })
         ];
     }
 
-    public setTokenPart(token: IndexedToken, part: Option) {
+    public setTokenPart(token: IndexedToken, option: MatrixOption) {
         if (this.isCustomXPath) {
             this.warningId = this.notificationService.add('It is not possible to use the matrix when using custom xpath.');
             return;
         }
 
-        if (this.tokenPartDisabled(token, part)) {
-            return;
-        }
+        this.matrix.rotate(token.index, option.key);
+        this.updateMatrix();
 
-        var updated = this.rotateValue(token.index, part);
-
-        this.emitChange({
-            attributes: updated
-        });
+        this.emitChange();
     }
 
-    public tokenPartDisabled(token: IndexedToken, part: Option) {
-        if (token.exclusive !== undefined && token.exclusive !== part.value) {
-            return true;
-        }
-
-        if (part.dependent_on) {
-            const dependent = this.attributes[token.index][part.dependent_on];
-            if (dependent === undefined || dependent === false) {
-                return true;
-            }
-        }
-
-        return false;
+    private updateMatrix() {
+        this.attributes = this.matrix.attributes;
+        let { advanced, dependents } = this.matrix.info();
+        this.alwaysAdvanced = advanced;
+        this.tokenDependents = dependents;
+        // hello change detection
+        this.indexedTokens = [...this.indexedTokens];
     }
 
     private emitChange(settings: Partial<MatrixSettings> = {}) {
@@ -201,7 +134,7 @@ export class MatrixComponent extends StepDirective<GlobalStateExampleBased> impl
             this.valid = true;
         }
         this.changeValue.next(Object.assign({
-            attributes: this.tokenValues,
+            attributes: [...this.matrix.attributes],
             retrieveContext: this.retrieveContext,
             customXPath: settings.customXPath || null,
             respectOrder: this.respectOrder,
@@ -259,34 +192,6 @@ export class MatrixComponent extends StepDirective<GlobalStateExampleBased> impl
         this.notificationService.cancel(this.warningId);
         this.subscriptions.forEach(s => s.unsubscribe());
         super.ngOnDestroy();
-    }
-
-    private rotateValue(tokenIndex: number, option: Option): TokenAttributes[] {
-        var value = (this.tokenValues[tokenIndex][option.value] ??
-            optionsLookup[option.value]);
-        var options: any[];
-
-        switch (option.type) {
-            case 'default':
-                options = ['include', 'exclude', undefined];
-                break;
-
-            case 'bool':
-                options = [true, false];
-                break;
-        }
-
-        var index = options.indexOf(value) + 1;
-        var update = options[index % options.length];
-
-        return [
-            ...this.tokenValues.slice(0, tokenIndex),
-            {
-                ...this.tokenValues[tokenIndex],
-                [option.value]: update
-            },
-            ...this.tokenValues.slice(tokenIndex + 1)
-        ];
     }
 }
 
---
+++ web-ui/src/app/models/matrix.spec.ts
@@ -0,0 +1,91 @@
+import { DefaultTokenAttributes, Matrix, TokenAttributes } from './matrix';
+
+describe('Matrix', () => {
+    let matrix: Matrix;
+    beforeEach(() => {
+        matrix = Matrix.default(5);
+    });
+
+    it('should set', () => {
+        expect(matrix.attributes[0].lemma).toEqual(undefined);
+        let changed = matrix.set(0, 'lemma', 'include');
+        expect(matrix.attributes[0].lemma).toEqual('include');
+        expect(changed).toEqual(true);
+
+        changed = matrix.set(0, 'lemma', 'include');
+        expect(changed).toEqual(false);
+    });
+
+    it('should handle na', () => {
+        let changed = matrix.set(0, 'na', true);
+        expect(changed).toEqual(true);
+        expect(matrix.attributes[0]).toEqual(<TokenAttributes>{
+            rel: undefined,
+            word: undefined,
+            lemma: undefined,
+            pos: undefined,
+            cs: false,
+            postag: undefined,
+            na: true
+        });
+
+        changed = matrix.set(0, 'na', false);
+        expect(changed).toEqual(true);
+        expect(matrix.attributes[0]).toEqual(DefaultTokenAttributes);
+    });
+
+    it('should handle case sensitive', () => {
+        matrix.set(0, 'cs', true);
+        expect(matrix.attributes[0].word).toEqual('include');
+
+        // case-sensitive should be switched off if word is switched off
+        matrix.set(0, 'word', undefined);
+        expect(matrix.attributes[0].cs).toEqual(false);
+        expect(matrix.attributes[0].word).toEqual(undefined);
+
+        // word should be switched on if case-sensitive is switched on
+        matrix.set(0, 'cs', true);
+        expect(matrix.attributes[0].cs).toEqual(true);
+        expect(matrix.attributes[0].word).toEqual('include');
+    });
+
+    it('setting word, should turn off only na', () => {
+        let changed = matrix.set(0, 'na', true);
+        expect(changed).toEqual(true);
+        expect(matrix.attributes[0]).toEqual(<TokenAttributes>{
+            rel: undefined,
+            word: undefined,
+            lemma: undefined,
+            pos: undefined,
+            cs: false,
+            postag: undefined,
+            na: true
+        });
+
+        changed = matrix.set(0, 'word', 'include');
+        expect(changed).toEqual(true);
+        expect(matrix.attributes[0]).toEqual({
+            rel: undefined,
+            word: 'include',
+            lemma: undefined,
+            pos: undefined,
+            cs: false,
+            postag: undefined,
+            na: false
+        });
+    });
+
+    it('setting everything else off, should turn on na', () => {
+        expect(matrix.attributes[0].na).toEqual(false);
+
+        matrix.set(0, 'rel', undefined);
+        matrix.set(0, 'lemma', undefined);
+        matrix.set(0, 'pos', undefined);
+        matrix.set(0, 'cs', false);
+        matrix.set(0, 'postag', undefined);
+        matrix.set(0, 'rel', undefined);
+        matrix.set(0, 'word', undefined);
+
+        expect(matrix.attributes[0].na).toEqual(true);
+    });
+});
