55014f9e90b546fa8f232d04f531bb18cdd7b33f
Tijmen Baarda <tijmenbaarda@gmail.com>
2022-08-01 15:53:51 +0200

    Retrieve treebanks from Django API - stage 2

--- backend/treebanks/urls.py
+++ backend/treebanks/urls.py
@@ -1,6 +1,6 @@
 from django.urls import include, path
 from rest_framework import routers
-from .views import TreebankViewset
+from .views import TreebankViewset, components_for_treebank_view
 
 router = routers.DefaultRouter()
 router.register(r'treebanks', TreebankViewset)
@@ -8,7 +8,11 @@ router.register(r'treebanks', TreebankViewset)
 urlpatterns = [
     path('', include(router.urls)),
     path(
+        'components-for-treebank/<slug:treebank>',
+        components_for_treebank_view
+    ),
+    path(
         'api-auth/',
         include('rest_framework.urls', namespace='rest_framework')
-    )
+    ),
 ]
--- backend/treebanks/views.py
+++ backend/treebanks/views.py
@@ -1,16 +1,23 @@
 from .models import Treebank, Component
 from .serializers import TreebankSerializer, ComponentSerializer
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 
-class TreebankViewset(viewsets.ModelViewSet):
+class TreebankViewset(viewsets.ReadOnlyModelViewSet):
     # TODO: make sure that non-public treesets are hidden if needed
     queryset = Treebank.objects.all().order_by('slug')
     serializer_class = TreebankSerializer
 
 
-class ComponentViewset(viewsets.ModelViewSet):
-    # TODO: make sure that components of non-public treebanks are hidden if
-    # needed
-    queryset = Component.objects.all().order_by('slug')
-    serializer_class = ComponentSerializer
+@api_view(['GET'])
+def components_for_treebank_view(request, treebank):
+    try:
+        treebank = Treebank.objects.get(slug=treebank)
+        # TODO: test if treebank is public and if not if it is accessible
+    except Treebank.DoesNotExist:
+        return Response(None, status=status.HTTP_404_NOT_FOUND)
+    components = Component.objects.filter(treebank=treebank)
+    serializer = ComponentSerializer(components, many=True)
+    return Response(serializer.data)
