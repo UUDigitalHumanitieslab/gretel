fa73995e5136637a14d77cc65c4438602aeb957e
Tijmen Baarda <tijmenbaarda@gmail.com>
2022-08-03 16:35:40 +0200

    Change API URLs and consistently use function-based views

--- backend/treebanks/views.py
+++ backend/treebanks/views.py
@@ -1,23 +1,25 @@
-from .models import Treebank, Component
+from .models import Treebank
 from .serializers import TreebankSerializer, ComponentSerializer
-from rest_framework import viewsets, status
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 
-class TreebankViewset(viewsets.ReadOnlyModelViewSet):
+@api_view(['GET'])
+def treebank_view(request):
     # TODO: make sure that non-public treesets are hidden if needed
-    queryset = Treebank.objects.all().order_by('slug')
-    serializer_class = TreebankSerializer
+    treebanks = Treebank.objects.all()
+    serializer = TreebankSerializer(treebanks, many=True)
+    return Response(serializer.data)
 
 
 @api_view(['GET'])
-def components_for_treebank_view(request, treebank):
+def treebank_components_view(request, treebank):
     try:
         treebank = Treebank.objects.get(slug=treebank)
         # TODO: test if treebank is public and if not if it is accessible
     except Treebank.DoesNotExist:
         return Response(None, status=status.HTTP_404_NOT_FOUND)
-    components = Component.objects.filter(treebank=treebank)
+    components = treebank.component_set
     serializer = ComponentSerializer(components, many=True)
     return Response(serializer.data)
