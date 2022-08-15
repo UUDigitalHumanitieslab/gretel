8b6abe4039a9a96e75ac65f8295586edb38fc685
Tijmen Baarda <tijmenbaarda@gmail.com>
2022-08-15 13:09:41 +0200

    Revert to SQLite for now

--- backend/gretel/settings.py
+++ backend/gretel/settings.py
@@ -84,10 +84,8 @@ WSGI_APPLICATION = 'gretel.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'OPTIONS': {
-            'read_default_file': str(BASE_DIR / 'mysql.cnf')
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
