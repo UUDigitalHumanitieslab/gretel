4558e93d3d6c7f8a6c057ec831e4407495af5875
ben <b.bonfil@uu.nl>
2022-08-25 10:45:15 +0200

    catch-all url redirect to home

--- backend/gretel/urls.py
+++ backend/gretel/urls.py
@@ -14,7 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,4 +25,6 @@ urlpatterns = [
     path('search/', include('search.urls')),
 
     path('mwe/', include('mwe.urls')),
+
+    re_path('^.*', RedirectView.as_view(url='/'), name='catch-all'),
 ]
